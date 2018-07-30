dcaae726f0e720beaeaecef2be4ec8570d6d459f
skoplev <skoplev@gmail.com>
2018-07-30 08:44:44 -0400

    Color consistency and order of tissues in module pie chart.

--- app/static/js/module.js
+++ app/static/js/module.js
@@ -9,18 +9,26 @@ $(document).ready(function() {
 });
 
 function renderModulePie(data) {
+	// Specify order of tissues for plotting
+	var tissue_order = ['AOR', 'MAM', 'VAF', 'SF', 'BLOOD', 'LIV', 'SKLM'];
+
 	// Count transcripts from certain tissues
+	// get array of transcript tissues
 	var tissue = data.map(function(row) {
 		return row['tissue']
 	})
-
-	// count using underscore.js
 	var tissue_counts = _.countBy(tissue);
 
+	// Reorder counts based on tissue_order
+	var tissue_counts_ordered = tissue_order.map(function(t) {
+		return tissue_counts[t];
+	});
+
 	pie_data = [{
-		values: Object.values(tissue_counts),
-		labels: Object.keys(tissue_counts),
-		type: 'pie'
+		values: tissue_counts_ordered,
+		labels: tissue_order,
+		type: 'pie',
+		sort: false  // dont reorder and recolor tissues
 	}];
 
 	Plotly.newPlot('tissue_pie', pie_data)
