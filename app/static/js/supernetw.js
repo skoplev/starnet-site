5d11446a0ce778dfee9dbc181ef9daa3c3cd1f08
skoplev <skoplev@gmail.com>
2018-08-01 17:46:31 -0400

    Supernetwork links to co-expresison module pages, indication of module ID on mouse hover.

--- app/static/js/supernetw.js
+++ app/static/js/supernetw.js
@@ -6,9 +6,9 @@ function superNetwork(data, id) {
 
 	// Plot dimensions
 	var width = 800,
-		height = 800;
+		height = 600;
 
-	var margin = 10;  // px
+	var margin = 50;  // px
 
 	var svg = d3.select(id).append('svg')
 		.attr('width', width)
@@ -49,17 +49,17 @@ function superNetwork(data, id) {
     // build arrows
     // see http://bl.ocks.org/d3noob/5141278 for adding data bindings
     svg.append("svg:defs")
-      .append("svg:marker")    // This section adds in the arrows
-        .attr("id", "end")
-        .attr("viewBox", "0 -5 10 10")
-        .attr("refX", 15)
-        .attr("refY", 0.0)
-        .attr("markerWidth", 5)
-        .attr("markerHeight", 5)
-        .attr("orient", "auto")
-      .append("svg:path")
-        .attr("d", "M0,-5L10,0L0,5")
-        .style("fill", "rgb(180,180,180)");
+ 		.append("svg:marker")    // This section adds in the arrows
+			.attr("id", "end")
+			.attr("viewBox", "0 -5 10 10")
+			.attr("refX", 15)
+			.attr("refY", 0.0)
+			.attr("markerWidth", 5)
+			.attr("markerHeight", 5)
+			.attr("orient", "auto")
+		.append("svg:path")
+			.attr("d", "M0,-5L10,0L0,5")
+			.style("fill", "rgb(180,180,180)");
 
 
 	// add lines for edges 
@@ -72,24 +72,38 @@ function superNetwork(data, id) {
 			.attr("y2", function(d) { return yscale(d.target.y); })
 			.attr("marker-end", "url(#end)");
 
-
+	// references focus which is defined below
 	var circle = svg.append("g").selectAll("circle")
 		.data(data.layout)
-		.enter().append("circle")
-			.attr("cx", function(d) {return xscale(d.x)})
-			.attr("cy", function(d) {return yscale(d.y)})
-			.attr("r", 3);
+		.enter().append("a")
+			// goto module page on click
+			.attr("xlink:href", function(d) {return "/module/" + d.module})
 
+			.append("circle")
+			.attr("cx", function(d) {return xscale(d.x); })
+			.attr("cy", function(d) {return yscale(d.y); })
+			.attr("r", 5)  // radius
+			.on("mouseover", function(d) {
+				d3.select(this).style("fill", "rgb(251,180,174)");
+				d3.select(this).style("r", 10);
+				focus.attr("transform", "translate(" + xscale(d.x) + "," + yscale(d.y) + ")");
+				focus.select("text").text(d.module);
+				focus.style("display", null);  // show
+			})
+			.on("mouseout", function(d) {
+				d3.select(this).style("fill", null);
+				d3.select(this).style("r", 5);
+				focus.style("display", "none");  // hide
+			});
 
-	// var path = svg.append("g").selectAll("path")
-	// 	.data(edges)
-	// 	.enter().append("path")
-	// 		.attr("d", linkArc);
+	// text information when hovering over nodes
+	var focus = svg.append("g")
+		.style("display", "none");
 
-	// function linkArc(d) {
-	// 	var dx = xscale(d.target.x) - xscale(d.source.x),
-	// 		dy = yscale(d.target.y) - yscale(d.source.y),
-	// 		dr = Math.sqrt(dx * dx + dy * dy);
-	// 	return "M" + xscale(d.source.x) + "," + yscale(d.source.y) + "A" + dr + "," + dr + " 0 0,1 " + xscale(d.target.x) + "," + yscale(d.target.y);
-	// }
+	focus.append("text")
+		// relative position of hover text
+		.attr("text-anchor", "middle")
+		.style("fill", "black")
+		.attr("dy", "-1em");
+
 }
