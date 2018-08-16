c2a62a4e1a9a3ae8d2da7bc6344fbb04651e92b4
Simon Koplev <koples01@minerva4.mgmt.hpc.mssm.edu>
2018-08-16 14:28:47 -0400

    Init of second test flask app on mod_wsgi Apache server.

---
+++ test/app2/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+# from flask_sqlalchemy import SQLAlchemy
+
+
+# Minimal test app for testing Apache web server
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	return "Yo from Flask, test App2!!!!"
+
+
