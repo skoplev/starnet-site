6ff5fdee7c98ca428a131e1c8ee46a9e6ac1e233
skoplev <skoplev@gmail.com>
2018-08-19 22:22:19 -0400

    Somewhat functional dynamic visualization of Bayesian networks on each co-expression module page.

--- app/static/js/module.js
+++ app/static/js/module.js
@@ -1,3 +1,66 @@
+// Global variables for dynamic network visualization of regulatory gene networks (Bayesian network)
+var height = 600;
+var width = 960;
+var radius = 5;
+
+// Init svg canvas
+var svg = d3.select("#rgn").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+// Variables storing current nodes and links
+var nodes = [];
+var links = [];
+
+// Force layout simulation
+var simulation = d3.forceSimulation(nodes)
+    .force("charge", d3.forceManyBody().strength(-100))
+    .force("link", d3.forceLink(links).distance(20))
+    .force("gravity", d3.forceManyBody().strength(20))
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    // .alphaTarget(1)
+    .on("tick", ticked);
+
+var g = svg.append("g")
+    link = g.append("g").attr("stroke", "#000").attr("stroke-width", 1.0).selectAll(".link"),
+    node = g.append("g").attr("stroke", "#fff").attr("stroke-width", 1.0).selectAll(".node");
+
+// update network based on nodes and links objects
+function updateNetwork() {
+	// Apply the general update pattern to the nodes.
+	node = node.data(nodes, function(d) { return d.id;});
+	node.exit().remove();
+	node = node.enter().append("circle")
+		.attr("fill", function(d) { return color(d.id); })
+		.attr("r", radius)
+		.merge(node);
+
+	// Apply the general update pattern to the links.
+	link = link.data(links, function(d) { return d.source.id + "-" + d.target.id; });
+	link.exit().remove();
+	link = link.enter().append("line").merge(link);
+
+	// Update and restart the simulation.
+	simulation.nodes(nodes);
+	simulation.force("link").links(links);
+	simulation.alpha(1).restart();
+}
+
+function ticked() {
+  node
+  	// Bounded by svg box
+	.attr("cx", function(d) { return Math.max(radius, Math.min(width - radius, d.x)); })
+	.attr("cy", function(d) { return Math.max(radius, Math.min(height - radius, d.y)); });
+
+  link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+}
+
+
 $(document).ready(function() {
 	// Get genes found in module
 	$.get('/api/module',
@@ -19,10 +82,14 @@ $(document).ready(function() {
 	$.get(url)
 		.done(function(data) {
 			// Parse data
-			// data = $.csv.toArrays(data);
 			data = $.csv.toObjects(data);
-			// console.log(data);
-			renderRGN(data);
+
+			var fdr_cutoff = calcFdrCutoff(data);
+			renderSlider(data, fdr_cutoff);
+
+			// Render network
+			setNetwork(data, fdr_cutoff);
+			updateNetwork();
 		}
 	);
 
@@ -264,32 +331,143 @@ function renderTableKDA(data) {
 	renderTable(data, "#kda_table", config);
 }
 
-function renderRGN(edges) {
-	var height = 600;
-	var width = 960;
-	var radius = 5;
-
+function calcFdrCutoff(data) {
+	// Calculate default selection
 	// Filter edges based on number of edges that can be shown
+	// Default filtering, can be changed by slider.
 	var max_edges = 300;
-	if (edges.length > max_edges) {
+	if (data.length > max_edges) {
 		// Assumes that edges data is ordered by kda_FDR
-		var fdr_cutoff = edges[max_edges - 1].kda_FDR
+		var fdr_cutoff = data[max_edges - 1].kda_FDR
+	} else {
+		fdr_cutoff == undefined;
+	}
 
+	return(fdr_cutoff)
+}
+
+function renderSlider(data, fdr_cutoff) {
+	// log transformed FDR values
+	var fdr_vals = data.map(function(d) { return -Math.log10(d.kda_FDR); })
+
+	var slider = d3.sliderHorizontal()
+	  .min(d3.min(fdr_vals))
+	  .max(d3.max(fdr_vals))
+	  .width(300)
+	  .tickFormat(d3.format('.3s'))
+	  .ticks(5)
+	  .default(-Math.log10(fdr_cutoff))
+	  .on('onchange', function(val) {
+	    setNetwork(data, Math.pow(10, -val));
+	    updateNetwork();
+	  });
+
+	  var g = d3.select("div#rgn_slider").append("svg")
+	    .attr("width", 500)
+	    .attr("height", 100)
+	    .append("g")
+	    .attr("transform", "translate(30,30)");
+
+	  g.call(slider);
+}
+
+
+
+// Updates state variables of dynamic networks
+function setNetwork(edges_data, fdr_cutoff) {
+
+	if (fdr_cutoff !== undefined) {
 		// Filter edges
-		edges_filter = edges.filter(function(elem) {
+		edges_filter = edges_data.filter(function(elem) {
 			return Number(elem.kda_FDR) < Number(fdr_cutoff);
 		});
 	} else {
-		// use all
-		edges_filter = edges;
+		edges_filter = edges_data;
 	}
 
 	var nodes_from = edges_filter.map(function(d) {return d.source; });
 	var nodes_to = edges_filter.map(function(d) {return d.target; });
 
-	var nodes = _.unique(nodes_from.concat(nodes_to));
+	// Get nodes as array if ids
+	var new_nodes = _.unique(nodes_from.concat(nodes_to));
 
-	nodes = nodes.map(function(d) { return {id: d }; })
+	// Get array of existing node ids
+	var node_ids = nodes.map(function(d) { return d.id })
+
+	// Only add new nodes
+	var add_nodes = _.difference(new_nodes, node_ids);
+
+	// remove nodes previously in network that are not specified
+	var del_nodes = _.difference(node_ids, new_nodes);
+
+	// delete nodes globally
+	del_nodes.map(function(id) {
+		index = node_ids.indexOf(id);
+		nodes.splice(index, 1);  // delete from array
+	})
+
+	// console.log(nodes);
+	// del_nodes = del_nodes.map(function(d) { return {id: d }; })
+
+
+	// Convert to node format, such that new nodes can be added
+	add_nodes = add_nodes.map(function(d) { return {id: d }; })
+
+	// Add new nodes permanently to global node variable
+	nodes = nodes.concat(add_nodes);
+
+
+	// get list of references to nodes array
+	// Edges only work if they are passed (by reference?) with respect to
+	// the objects in the global nodes variable
+	var node_ids = nodes.map(function(d) { return d.id })
+
+	var new_edges = edges_filter.map(function(edge) {
+		var source_index = node_ids.indexOf(edge.source);
+		var target_index = node_ids.indexOf(edge.target);
+		return {source: nodes[source_index], target: nodes[target_index]}
+	});
+
+	// Get unique string IDs of present edges
+	links_ids = links.map(edgeID);
+
+	// Only add edges that are not already present in network
+	new_edges = new_edges.filter(function(edge) {
+		return links_ids.indexOf(edgeID(edge)) === -1;
+	});
+
+
+	// Edges to remove
+	var del_edges = _.difference(links_ids, new_edges.map(edgeID));
+
+	// Delete edges
+	del_edges.map(function(id) {
+		var index = links_ids.indexOf(id);
+		links.splice(index, 1);
+	});
+
+	// add new edges
+	links = links.concat(new_edges);
+
+	// links = new_edges;
+
+	// restart()
+
+}
+
+// Generates unique string ID for network edges
+function edgeID(edge) {
+	return edge.source.id + "-" + edge.target.id;
+}
+
+// Old function for rendering static network
+// var nodes = [];
+// var edges_filter = [];
+function renderRGN(edges) {
+	// console.log(edges);
+
+	var height = 600;
+	var width = 960;
 
 	// Init network layout simulation
 	var simulation = d3.forceSimulation()
@@ -303,15 +481,64 @@ function renderRGN(edges) {
 		.attr("width", width)
 		.attr("height", height);
 
-	var link = svg.append("g")
-		.attr("class", "links")
+	var line = svg.append("g")
+		.attr("id", "links")
+		.attr("class", "links");
+
+	var node = svg.append("g")
+		.attr("id", "nodes")
+		.attr("class", "nodes");
+
+	function ticked() {
+	  link
+	      .attr("x1", function(d) { return d.source.x; })
+	      .attr("y1", function(d) { return d.source.y; })
+	      .attr("x2", function(d) { return d.target.x; })
+	      .attr("y2", function(d) { return d.target.y; });
+
+	  node
+	      .attr("cx", function(d) { return Math.max(radius, Math.min(width - radius, d.x)); })
+	      .attr("cy", function(d) { return Math.max(radius, Math.min(height - radius, d.y)); });
+	}
+
+	fdr_cutoff = calcFdrCutoff(edges)
+	renderNetwork(svg, simulation, edges, fdr_cutoff);
+}
+
+
+// Old function for rendering static network
+function renderNetwork(svg, simulation, edges, fdr_cutoff=undefined) {
+	var radius = 5;
+
+	// Get svg dimensions
+	var width = svg.style("width").replace("px", "");
+	var height = svg.style("height").replace("px", "");
+
+
+	if (fdr_cutoff !== undefined) {
+		// Filter edges
+		edges_filter = edges.filter(function(elem) {
+			return Number(elem.kda_FDR) < Number(fdr_cutoff);
+		});
+	} else {
+		edges_filter = edges;
+	}
+
+	var nodes_from = edges_filter.map(function(d) {return d.source; });
+	var nodes_to = edges_filter.map(function(d) {return d.target; });
+
+	var nodes = _.unique(nodes_from.concat(nodes_to));
+
+	nodes = nodes.map(function(d) { return {id: d }; })
+
+
+	var link = svg.select("#links")
 		.selectAll("line")
 		.data(edges_filter)
 		.enter().append("line")
 			.attr("stroke-width", 2);
 
-	var node = svg.append("g")
-		.attr("class", "nodes")
+	var node = svg.select("#nodes")
 		.selectAll("circles")
 		.data(nodes)
 		.enter().append("circle")
@@ -359,3 +586,5 @@ function renderRGN(edges) {
 		d.fy = null;
 	}
 }
+
+
